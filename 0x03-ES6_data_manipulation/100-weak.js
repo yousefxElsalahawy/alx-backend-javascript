9b8c26e9bb98b78eee2c8d35a9993f4258998237
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-03 02:13:51 +0300

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,24 +1,24 @@
-// export const weakMap = new WeakMap();
-
-// // https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
-// export function queryAPI(endpoint) {
-//   let called = 0;
-//   if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
-//   weakMap.set(endpoint, called + 1);
-//   if (called + 1 >= 5) throw new Error('Endpoint load is high');
-// }
-
-
 export const weakMap = new WeakMap();
 
-export const queryAPI = (endpoint) => {
-  if (weakMap.has(endpoint)) {
-    const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 4) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, (endpointData + 1));
-  } else {
-    weakMap.set(endpoint, 1);
-  }
-};
+// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
+export function queryAPI(endpoint) {
+  let called = 0;
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
+}
+
+
+// export const weakMap = new WeakMap();
+
+// export const queryAPI = (endpoint) => {
+//   if (weakMap.has(endpoint)) {
+//     const endpointData = weakMap.get(endpoint);
+//     if (endpointData >= 4) {
+//       throw new Error('Endpoint load is high');
+//     }
+//     weakMap.set(endpoint, (endpointData + 1));
+//   } else {
+//     weakMap.set(endpoint, 1);
+//   }
+// };

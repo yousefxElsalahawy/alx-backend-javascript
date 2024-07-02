08027728b115367dedd06f1aad63d3b3e1a133f4
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-03 02:14:53 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,39 @@
 
-export default getListStudents = () => {
+// export default getListStudents = () => {
 
-  let id = [1,2 ,5];
-  let firstName = ["John", "Jane", "Serena"];
-  let location =[ "London", "Columbia", "San Francisco"];
-  let info =[]
-  for(let i = 0; i < id.length; i++){
-    info.push({
-      id : id[i],
-      firstName : firstName[i],
-      location : location[i]
-    })
-  }
-  return info
-}
+//   let id = [1,2 ,5];
+//   let firstName = ["John", "Jane", "Serena"];
+//   let location =[ "London", "Columbia", "San Francisco"];
+//   let info =[]
+//   for(let i = 0; i < id.length; i++){
+//     info.push({
+//       id : id[i],
+//       firstName : firstName[i],
+//       location : location[i]
+//     })
+//   }
+//   return info
+// }
+
+
+
+
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+export default getListStudents;

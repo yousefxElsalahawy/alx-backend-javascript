616d6146835b999e2be32638780cf9b965191aed
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-03 02:17:16 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,17 @@
-export default function getListStudentIds(list) {
-  if (Array.isArray(list)) {
-    return list.map((obj) => obj.id);
+// export default function getListStudentIds(list) {
+//   if (Array.isArray(list)) {
+//     return list.map((obj) => obj.id);
+//   }
+//   return [];
+// }
+
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
   }
-  return [];
-}
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
+
+export default getListStudentIds;

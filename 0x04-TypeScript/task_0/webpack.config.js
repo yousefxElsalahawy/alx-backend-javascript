c172f44f36277d8cf4de49556e5c73b0b516779a
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-03 23:09:41 +0300

    webpack.config.js

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,37 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+// Write an interface named Student that accepts the following elements: firstName(string), lastName(string), age(number), and location(string)
+// Create two students, and create an array named studentsList containing the two variables
+// Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
+// Each row should contain the first name of the student and the location
 
-module.exports = {
-  entry: "./js/main.ts",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  devServer: {
-    contentBase: "./dist"
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Development"
-    })
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
-};
+// Define the Student interface
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+// Create two student objects
+const firstStudent: Student = {
+	firstName: "Obed",
+	lastName: "Ehoneah",
+	age: 97,
+	location: "Accra"
+}
+
+const secondStudent: Student = {
+	firstName: "Frank",
+	lastName: "Ehoneah",
+	age: 79,
+	location: "Kumasi"
+}
+
+// Create an array of students
+const studentsList = [firstStudent, secondStudent];
+
+const table = document.getElementById('studentTable') as HTMLTableElement;
+
+// Create a new row for each student and append it to the table
+studentsList.forEach((student) => {
+	const row = table.insertRow();
+	const firstNameCell = row.insertCell(0);
+	const locationCell = row.insertCell(1);
+
+	firstNameCell.innerHTML = student.firstName;
+	locationCell.innerHTML = student.location;
+})

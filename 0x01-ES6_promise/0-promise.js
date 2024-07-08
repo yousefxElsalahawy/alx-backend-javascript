5ec19e070c72f1eb51b2213dc9db12e327a62264
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-08 19:16:38 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,8 @@
+
 /* eslint-disable */
-export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
-}
+
+export default function getResponseFromAPI(){
+  return new Promise((resolve,reject) => {
+    
+  })
+};

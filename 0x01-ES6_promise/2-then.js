7d8023913a97dabff8fc11ecde832d3cd4097515
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-08 20:49:41 +0300

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
-export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => { console.log('Got a response from the API'); });
+export default function handleResponseFromAPI(promise){
+  return promise.then(() => console.log("{ status: 200, body: 'Success' }")).catch(() => new Error("{}")).finally(() => console.log("Got a response from the API"))
 }
+
+
+

058d70754f651a5df386264a53f2863a068719bb
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-08 19:31:32 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success){
+  return new promise ((res, rej) => {
+    if (success){
+      res({ status: 200, body: 'Success' });
+    }else{
+      rej(new Error("The fake API is not working currently"));
     }
   });
 }

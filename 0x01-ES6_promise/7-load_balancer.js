1def45382970cf6b73f4dfeb9837159a59014b5e
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-09 16:17:08 +0300

    Update 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
     return Promise.race([chinaDownload, USDownload]).then((res) => res)
 }
-//     setTimeout(resolve, 100, ukSuccess);
-//      resolve >> function , 100 >> wait time , ukSuccess >> value given to func resolve

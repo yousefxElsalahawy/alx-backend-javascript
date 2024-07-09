9c62e2f8b64a8e075b3eb444daf0685175302d67
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-09 18:09:39 +0300

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,22 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(`Error: ${error.message}`);
-  } finally {
-    queue.push('Guardrail was processed');
+  const queue =[];
+
+  try{
+    let value = mathFunction()
+    queue.push(value)
   }
-  return queue;
+  catch(error){
+    /* <<error>>:
+قد يتضمن معلومات إضافية مثل stack trace.
+قد يكون غير واضح عند عرضه.
+<<error.message>>:
+يتضمن فقط رسالة الخطأ النصية.
+أكثر وضوحًا ومباشرة. */
+    queue.push(`Error: ${error.message}`)
+  }
+  finally{
+    queue.push("Guardrail was processed")
+  }
+  return queue
 }
+

7e6637827c95081a73f6d89f3b995249ee3200d2
yousefxElsalahawy <133533423+yousefxElsalahawy@users.noreply.github.com>
2024-07-09 15:55:19 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,8 @@
-mport signUpUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName ) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res)=> res.map((rest) => {return{
-    status : rest.status,
-    value : rest.value || rest.reason
+  r
   }}))
 }
 
